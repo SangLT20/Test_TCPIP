37c86a1deaa86ce72de694786e85068c0b691412
SangLT20 <196letansang@gmail.com>
2023-03-05 09:15:58 +0700

    create project

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        const int PORT_NO = 5000;
+        const string SERVER_IP = "127.0.0.2";
+        static void Main(string[] args)
+        {
+            //---create a TCPClient object at the IP and port no.---
+            TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+            NetworkStream nwStream = client.GetStream();
+
+            while (true)
+            {
+                //---data to send to the server---
+                string textToSend = DateTime.Now.ToString();
+                textToSend = $"{Console.ReadLine()} : {textToSend} ";
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+                //---send the text---
+                Console.WriteLine("Sending : " + textToSend);
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+                //---read back the text---
+                byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+
+            }
+
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
